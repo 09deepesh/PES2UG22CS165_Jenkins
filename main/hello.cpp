2d7400b45f5072602d1e843b237fd7164964172a
09deepesh <151416825+09deepesh@users.noreply.github.com>
2025-03-14 16:03:44 +0530

    Add files via upload

---
+++ main/hello.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << " Myself Deepesh(PES2UG22CS165) I have successfully built and run this application   \n";
+  return 0;
+}
