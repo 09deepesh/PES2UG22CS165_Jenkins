a3221eb15cd4a868fe8928e760ed786e175f19cd
09deepesh <151416825+09deepesh@users.noreply.github.com>
2025-03-18 16:34:40 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << " Myself PES2UG22CS165 I have successfully built and run this application and i have changed it  \n";
+  return 0;
+}
